e13966ae0b225fb2d09c04b5901125f4c31c8436
UHMWPE <isaac.chan@mail.mcgill.ca>
2018-02-11 18:32:46 -0500

    Created appropriate config on Isaac's laptop

--- LAB2/RTE/_Target_1/RTE_Components.h
+++ LAB2/RTE/_Target_1/RTE_Components.h
@@ -16,6 +16,10 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_ADC
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DAC
 #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
 
 #endif /* RTE_COMPONENTS_H */
